0c5e25d0febaaba73619e35dccf88fdd59fbaa6a
naama <0naama0@gmail.com>
2020-06-26 13:43:09 -0400

    added reactstrap and created component files

---
+++ project2/src/Components/Header/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'Nav.css';
+
+const Nav = () => {
+
+    return (
+        <nav>
+            <div>Icon</div>
+            <div>Synonym</div>
+            <div>Antonym</div>
+            <div>Definition</div>
+            <div><h1>Guess The Word</h1></div>
+            <div>Leaderboard</div>
+        </nav>
+    )
+}
