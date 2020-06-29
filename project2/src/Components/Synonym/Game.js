fc3f84f3ccc249a44cbb25d3a777c4790b6b09a3
naama <0naama0@gmail.com>
2020-06-29 03:04:09 -0400

    updated game component

--- project2/src/Components/Synonym/Game.js
+++ project2/src/Components/Synonym/Game.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import WordList from './WordList';
 
 const Game = () => {
-    // const [arrayOfWords, setArrayOfWords] = useState([]);
-    const randomWords = [];
-    // const [objectOfWords, setObjectOfWords] = useState({})
-
+    const [arrayOfWords, setArrayOfWords] = useState([]);
+    // const randomWords = [];
+    const [objectOfWords, setObjectOfWords] = useState({})
+    const [randomWords, setRandomWords] = useState([])
     // function renderData(apiOne, apiTwo){
     //     console.log("synonyms - ", apiTwo)
     //     let things = apiTwo.map((item, i)=>{
@@ -14,49 +14,43 @@ const Game = () => {
     //         )
     //     })
     // }
-    function apiCallOne(){
-        fetch('https://raw.githubusercontent.com/junaama/word-guesser/master/random-word.json')
-            .then(res => res.json())
-            .then(resOne => {
-                let tally = 0;
-                while (tally < 30){
-                    let randomNum = Math.floor(Math.random()* 2982);
-                    let newWord = resOne[randomNum]
-                    randomWords.push(newWord)
-                    tally++;
-                }
-                console.log(randomWords)
-                return randomWords
-                    //  getSynonyms(),
-                    // ,
-                    // renderData(resOne, resTwo)
-                // )
-            });
-            
+
+  async function apiCallOne(){
+        // fetch('https://raw.githubusercontent.com/junaama/word-guesser/master/random-word.json')
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         let tally = 0;
+        //         while (tally < 30){
+        //             let randomNum = Math.floor(Math.random()* 2982);
+        //             let newWord = data[randomNum];
+        //             // randomWords.push(newWord);
+        //             //setRandomWords([...randomWords, newWord])
+        //             tally++;
+        //         }
+        //         console.log(randomWords);
+        //         return randomWords;
+        //     });
+            const res = await fetch('https://raw.githubusercontent.com/junaama/word-guesser/master/random-word.json');
+            const data = await res.json();
+            let tally=0;
+            while(tally < 30){
+                let randomNum = Math.floor(Math.random()* 2982);
+                let newWord = data[randomNum];
+                // randomWords.push(newWord);
+                //setRandomWords(newWord)
+                tally++;
+            }
+
     }
-    // function apiCallTwo(){
-    //     fetch(`https://wordsapiv1.p.rapidapi.com/words/ugly/synonyms`, {
-    //         headers: {
-    //             "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-    //             "x-rapidapi-key": "4c9c4d7bdbmsh5c0c6dfda6f586dp172c76jsn1f5c896e75d7",
-    //         },
-    //     })
-    //         .then(res => {
-    //             return (
-    //             res.json()
-    //             .then((data) => {return data.synonyms})
-    //             )
-    //         })
-    //         .then(resTwo => {
-    //             apiCallOne()
-    //         });
-    // };
 
-
+//loops not working?
   function apiCallTwo(){
       let objectOfWords = {};
-      for(let i = 0; i < randomWords.length; i++){
-          let word = randomWords[i];
+      console.log(randomWords)
+    //   for(let i = 0; i < randomWords.length; i++){
+          let word = randomWords[1];
+          console.log(word)
+ 
           fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}/synonyms`, {
             headers: {
                 "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
@@ -65,30 +59,43 @@ const Game = () => {
         })
             .then(res => res.json())
             .then((data) => {
-                // console.log(data)
+                 console.log(data)
                  objectOfWords[word] = data.synonyms;
                 //setObjectOfWords({...objectOfWords, data})
                 //setObjectOfWords({word: data.synonyms})
                 //setArrayOfWords(data)
                 })
-            // })
-      }//close for loop
+    //   }
       console.log(objectOfWords)
       return objectOfWords;
     }
-    
+    useEffect(()=> {
+        apiCallOne();
+    },[])
+    useEffect(()=> {
+        apiCallOne();
+    },[randomWords])
+
     console.log(randomWords)
 
-    
+    if(!randomWords[0]){
+        console.log('bye')
+    }else {
+        apiCallTwo();
+    }
+
+    // useEffect(()=>{
+    //     apiCallTwo()
+    // }, [])
 
     return (
         <div>
             {/* <WordList listOfWords={listOfWords}/> */}
             {/* {apiCallTwo()} */}
-            Hi
+            In Game
             {/* {randomWords} */}
           
-           <WordList />
+           {/* <WordList /> */}
         </div>
         
     )
