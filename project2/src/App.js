e372a80f235eed162b259ff1095b9604e72b9f2a
naama <0naama0@gmail.com>
2020-06-29 23:02:04 -0400

    cleaned up code

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,18 +1,14 @@
 import React, { useState } from 'react';
 import Form from './Components/Form/Form';
 import Header from './Components/Header/Header';
-
 import './App.css';
 
 function App() {
   
-
-
   return (
     <div className="App">
-     <h1>I Have A Word For That...</h1>
+     <h1>Word Finder</h1>
      <Header />
-     {/* <Form /> */}
     </div>
   );
 }
--- project2/src/Components/Form/Form.js
+++ project2/src/Components/Form/Form.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from "react";
-import Synonym from '../Synonym/Synonym';
-import Antonym from '../Antonym/Antonym';
-import MeansLike from '../MeansLike/MeansLike';
-import Homophone from '../Homophone/Homophone';
-import Rhyme from '../Rhyme/Rhyme';
-import {Link, Route} from 'react-router-dom';
+
 //https://www.carlrippon.com/drop-down-data-binding-with-react-hooks/
+
 const Form = () => {
   let [input, setInput] = useState("");
   let [data, setData] = useState([]);
   let [constraint, setConstraint] = useState("");
   let handler = constraint;
-  //
+  let results;
+  let mlResults;
+  let antResults;
+  let synResults;
+  let rhyResults;
+  let homResults;
+
   const makeApiCall = async () => {
     const url = `https://api.datamuse.com/words?${constraint}=${input}`;
-    console.log("in api call -", url);
     const res = await fetch(url);
     const data = await res.json();
     setData(data);
@@ -22,69 +23,45 @@ const Form = () => {
 
   useEffect(() => {
     makeApiCall();
-    console.log(data);
   }, [input]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
   const handleChange = (e) => {
-    console.log(e.target.value);
     setInput(e.target.value);
   };
   const handleSelectChange = (e) => {
-    console.log("inside handle select - ", e.target.value);
     setConstraint(e.target.value);
   };
 
-  let results;
-
-  let [mlResults, setMlResults] = useState();
-  let [antResults, setAntResults] = useState();
-  let [synResults, setSynResults] = useState();
-  let [rhyResults, setRhyResults] = useState();
-  let [homResults, setHomResults] = useState();
-/*
-Previous way
-results = data.map((item, i) => {
-console.log(item);
-return <p key={i}>Similar word: {item.word}</p>;
-})
-*/
   if (constraint) {
     switch (handler) {
       case "ml":
-        console.log(" in switch - ml");
         mlResults = data.map((item, i) => {
-        console.log(item);
         return <p key={i}>Similar word: {item.word}</p>;
         })
         break;
       case "rel_ant":
-        console.log(" in switch - rel_ant");
-        results = data.map((item, i) => {
-            console.log(item);
+        antResults = data.map((item, i) => {
+            if(!item){
+              console.log("dne")
+            }
             return <p key={i}>Antonym: {item.word}</p>;
             })
         break;
       case "rel_syn":
-        console.log(" in switch - rel_syn");
-        results = data.map((item, i) => {
-            console.log(item);
+        synResults = data.map((item, i) => {
             return <p key={i}>Synonym: {item.word}</p>;
             })
         break;
       case "rel_rhy":
-        console.log(" in switch - rel_rhy");
-        results = data.map((item, i) => {
-            console.log(item);
+        rhyResults = data.map((item, i) => {
             return <p key={i}>Rhyme: {item.word}</p>;
             })
         break;
       case "rel_hom":
-        console.log(" in switch - rel_hom");
-        results = data.map((item, i) => {
-            console.log(item);
+        homResults = data.map((item, i) => {
             return <p key={i}>Homophone: {item.word}</p>;
             })
         break;
@@ -109,7 +86,8 @@ return <p key={i}>Similar word: {item.word}</p>;
         </select>
         <input type="submit" value="Search" />
       </form>
-      Results: {results}
+      Results: {mlResults} {synResults} {antResults} {rhyResults} {homResults}
+      
     </div>
   );
 };
