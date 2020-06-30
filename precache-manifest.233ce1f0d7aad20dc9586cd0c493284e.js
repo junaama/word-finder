c9be6726b0316f7a3538b303b2800dbbb2fb944a
naama <0naama0@gmail.com>
2020-06-30 14:14:18 -0400

    Updates

---
+++ precache-manifest.233ce1f0d7aad20dc9586cd0c493284e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e2f068773f6e44ea8200d1ccd86050",
+    "url": "/word-finder/index.html"
+  },
+  {
+    "revision": "598311384e9b74c1e8fa",
+    "url": "/word-finder/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8387edc091a25cf8556b",
+    "url": "/word-finder/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "598311384e9b74c1e8fa",
+    "url": "/word-finder/static/js/2.6c23cecf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/word-finder/static/js/2.6c23cecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8387edc091a25cf8556b",
+    "url": "/word-finder/static/js/main.6157e6df.chunk.js"
+  },
+  {
+    "revision": "00f3f2d5feb1a53c1697",
+    "url": "/word-finder/static/js/runtime-main.d6590f9c.js"
+  }
+]);
