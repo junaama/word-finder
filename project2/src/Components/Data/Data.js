218c6ca9385b5cb05f0efd871611a6e53107e26a
naama <0naama0@gmail.com>
2020-06-29 20:11:31 -0400

    rendered results on page

--- project2/src/Components/Data/Data.js
+++ project2/src/Components/Data/Data.js
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from "react";
-
+//https://www.carlrippon.com/drop-down-data-binding-with-react-hooks/
 const Data = () => {
   let [input, setInput] = useState("");
   let [data, setData] = useState([]);
+  let [constraint, setConstraint] = useState("");
+  let handler = constraint;
+//  
   const makeApiCall = async () => {
-    const url = `https://api.datamuse.com/words?ml=${input}`;
+    const url = `https://api.datamuse.com/words?${constraint}=${input}`;
+    console.log("in api call -", url)
     const res = await fetch(url);
     const data = await res.json();
     setData(data);
   };
 
   useEffect(() => {
-    makeApiCall(input);
+    makeApiCall();
+    console.log(data)
   }, [input]);
 
   const handleSubmit = (e) => {
@@ -21,21 +26,38 @@ const Data = () => {
     console.log(e.target.value);
     setInput(e.target.value);
   };
+  const handleSelectChange = (e)=> {
+      console.log("inside handle select - ", e.target.value)
+      setConstraint(e.target.value)
+  }
+  let results;
+ if(data[0]){
+     results = data.map((item,i)=>{
+        console.log(item)
+        return(
+            <p>Similar word: {item.word}</p>
+        )
+     })
+ }
+ 
   return (
     <div>
-      Hello in Data
+      Hello in Data - I have a word for that
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange}></input>
+        <br/>
         <label htmlFor="cars">Dropdown: </label>
-        <select name="cars" id="cars">
-          <option>Find Definition</option>
-          <option>Find Synonyms</option>
-          <option>Find Antonyms</option>
-          <option>Find Rhymes</option>
-          <option>Find Homophones</option>
+        <select name="options" id="options" onChange={handleSelectChange}>
+            <option value="null">Select an option</option>
+          <option value="ml">Find Meaning Similar To</option>
+          <option value="rel_syn">Find Synonyms</option>
+          <option value="rel_ant">Find Antonyms</option>
+          <option value="rel_rhy">Find Rhymes</option>
+          <option value="rel_hom">Find Homophones</option>
         </select>
-        <input type="submit" />
+        <input type="submit" value="Submit"/>
       </form>
+      Results: {results}
     </div>
   );
 };
